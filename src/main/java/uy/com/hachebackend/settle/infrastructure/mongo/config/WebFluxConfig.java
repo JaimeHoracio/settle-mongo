231660ae5a1b2189eae2ed4723456a22893a1738
JaimeHoracio <solopagina@gmail.com>
2023-05-28 14:32:42 -0300

    Revierto conexion reactiva con configuracion personalizada de Mongo

--- src/main/java/uy/com/hachebackend/settle/infrastructure/mongo/config/WebFluxConfig.java
+++
@@ -1,9 +0,0 @@
-package uy.com.hachebackend.settle.infrastructure.mongo.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.reactive.config.EnableWebFlux;
-
-@Configuration
-@EnableWebFlux
-public class WebFluxConfig {
-}
